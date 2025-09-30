b5249b9857ff438883ddd7e77be43db52799e293
Ratan <ratanrathod870@gmail.com>
2025-09-30 19:40:44 +0530

    fixed footer text size on mobile

--- components/footerText.tsx
+++ components/footerText.tsx
@@ -4,7 +4,7 @@ import DecryptedText from './ui/DecryptedText'
 const FooterText = () => {
     return (
         <>
-            <div className='relative leading-none tracking-tighter mt-14  flex items-center justify-center text-[8vw] uppercase'>
+            <div className='relative leading-tight md:w-full w-screen tracking-tighter mt-14  flex items-center justify-center text-6xl md:text-[8vw] uppercase'>
                 <DecryptedText text='Ratan Rathod' />
                 <div className='absolute bg-white -bottom-1 blur-2xl h-10 w-full ' />
             </div>
